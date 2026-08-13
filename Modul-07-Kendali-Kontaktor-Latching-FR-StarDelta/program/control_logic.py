85383e4afbb222eca9053569784fced4ee709c56
rofiq cahyo prayogo <31286539+rofiqcp@users.noreply.github.com>
2026-08-14 01:20:35 +0700

    clarify P07 simulation output wording

--- Modul-07-Kendali-Kontaktor-Latching-FR-StarDelta/program/control_logic.py
+++ Modul-07-Kendali-Kontaktor-Latching-FR-StarDelta/program/control_logic.py
@@ -16,6 +16,12 @@ class StarDelta:
         assert not (star and delta)
         return main,star,dead,delta
 
-c=FRController(); c.start_fwd(); c.start_rev(); print('FWD/REV=',c.fwd,c.rev,'valid=',c.valid()); c.stop(); c.start_rev(); print('after stop REV=',c.rev)
+c=FRController()
+c.start_fwd(); c.start_rev()
+print('FWD request then REV request while FWD active =',c.fwd,c.rev,'valid=',c.valid())
+c.stop()
+print('after STOP =',c.fwd,c.rev)
+c.start_rev()
+print('new REV request after STOP =',c.fwd,c.rev,'valid=',c.valid())
 s=StarDelta()
 for t in [0,1,2.9,3.05,3.2,5]: print(t,s.state(t))
